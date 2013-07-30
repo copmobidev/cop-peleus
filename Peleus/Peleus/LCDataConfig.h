02ce33f0e8b8ab24e3b0685de95fe4bdea996d30
xuweiyan <macknight2013@gmail.com>
2013-07-30 08:09:55 +0800

    api server

--- Peleus/Peleus/LCDataConfig.h
+++ Peleus/Peleus/LCDataConfig.h
@@ -6,5 +6,9 @@
 //  Copyright (c) 2013 cop-studio. All rights reserved.
 //
 
-#define COP_BIZ_SERVER      @"http://58.210.101.202:59102"
-#define COP_OBD_SERVER      @"http://192.168.0.1"
+//host is only host part, do not include protocol , port and detail path
+
+//#define COP_BIZ_SERVER      @"58.210.101.202:59102"
+
+#define COP_BIZ_SERVER      @"58.210.101.202"
+#define COP_OBD_SERVER      @"192.168.111.1"
